81c284f620e8635a0e98d40e257aba0dfa88f8ad
Miko <s.zhanar173@gmail.com>
2025-02-06 20:01:19 +0500

    Add files via upload

---
+++ lesson-6/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Lesson-6</title>
+</head>
+<body>
+<div>
+    
+  <?php  
+  
+  // $args = ['Anna', 10, 5.66 ,false,[10, true, 'hi']]; Массив тип записи 1
+  // $args = array('Anna', 10, 5.66 ,false);            Массив тип записи 2
+  // $args = array('Anna', 10, 5.66 ,false);
+  // var_dump($args[3]);
+
+  
+  // $argc = [
+  //   'name'=> 'Alex',
+  //   'age' => 15,
+  //   'is_male' => true,
+  //   'items' => ['phone, table', 'laptop'],
+  // ];
+
+  // var_dump($argc ['name']);
+
+  
+  // // $int = 'WORLD';
+  // $int = ['HII', 'WOrld'];
+  // echo $int[1] . $int[0]; 
+
+  
+  // $var = 'world';
+  // echo "HELLO {$var}s";
+
+  // echo "HELLO \"Little\" World";
+
+  // echo 2 + 2;
+
+for ($i=0; $i <= 10; $i++) { 
+  echo "<span class='span-$i'> LOL", '<br>';
+}
+
+  ?>
+
+
+
+  
+</div>
+</body>
+</html>
